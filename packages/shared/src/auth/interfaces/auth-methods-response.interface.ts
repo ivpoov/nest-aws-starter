a84d23c1d2be346d5bfde845a20e14b8e5f2825e
Igor Popov <ivpoov@gmail.com>
2026-08-03 16:04:10 +0300

    feat(shared): add auth method wire contracts

---
+++ packages/shared/src/auth/interfaces/auth-methods-response.interface.ts
@@ -0,0 +1,5 @@
+import type { AuthMethodResponseInterface } from './auth-method-response.interface.js';
+
+export interface AuthMethodsResponseInterface {
+  readonly methods: AuthMethodResponseInterface[];
+}
