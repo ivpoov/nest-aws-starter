61f3b222c5db90d6712820dec1797e605a728e4c
Igor Popov <ivpoov@gmail.com>
2026-08-04 04:46:09 +0300

    feat(shared): add api-key wire contracts

---
+++ packages/shared/src/api-keys/interfaces/create-api-key-request.interface.ts
@@ -0,0 +1,3 @@
+export interface CreateApiKeyRequestInterface {
+  readonly name: string;
+}
---
+++ packages/shared/src/api-keys/types/api-key-error-code.type.ts
@@ -0,0 +1,3 @@
+import type { API_KEY_ERROR_CODES } from '../constants/api-key-error-codes.constants.js';
+
+export type ApiKeyErrorCodeType = (typeof API_KEY_ERROR_CODES)[number];
